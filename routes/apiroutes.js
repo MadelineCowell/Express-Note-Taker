9c4c7ad0556ae6da5dc359b7afb5f45da81b146e
MadelineC <madeline.cowell@colorado.edu>
2020-10-30 12:31:40 -0600

    Updated api routes, but still need to fix

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -3,14 +3,19 @@ const action = require("../db/action");
 
 // use to get API Routes (get/post/delete)
 
+//GET
 router.get("/notes", function (req, res) {
     action.getNotes().then((notes) => res.json(notes))
 });
 
+// POST(CREATE)
 router.post("/notes", function (req, res) {
     action.addNote().then((notes) =>res.json(notes))
 })
 
-router.delete("/notes", function (req, res))
+//DELETE - NEED TO FIX
+router.delete("/notes", function (req, res) {
+    action.deleteNote().then(() => res.json(notes))
+})
 
 module.exports = router;
